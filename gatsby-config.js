26a95fd160dfc94ad340c0aa642a33f55b1bf3c4
andregm <andre@appdev.com.br>
2020-05-04 17:42:55 -0300

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ if (!spaceId || !accessToken) {
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Contentful starter',
+    title: 'Andre Gomes Web Dev',
   },
   pathPrefix: '/gatsby-contentful-starter',
   plugins: [
